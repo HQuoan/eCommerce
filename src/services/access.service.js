50e01c594f01fd65be6c6d973f0795fc05ec2b0a
Vo Minh Huy <huyvodtan@gmail.com>
2024-03-25 18:01:29 +0700

    ss5_p2 signup v2 simple

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -39,42 +39,29 @@ class AccessService {
 
       if (newShop) {
         // created privateKey, publicKey
-        const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-          modulusLength: 4096,
-          publicKeyEncoding: {
-            type: 'pkcs1',
-            format: 'pem',
-          },
-          privateKeyEncoding: {
-            type: 'pkcs1',
-            format: 'pem',
-          },
-        });
+        const privateKey = crypto.randomBytes(64).toString('hex');
+        const publicKey = crypto.randomBytes(64).toString('hex');
 
         console.log({ privateKey, publicKey });
 
         // save publicKey in db
-        const publicKeyString = await KeyTokenService.createKeyToken({
+        const keyStore = await KeyTokenService.createKeyToken({
           userId: newShop._id,
           publicKey,
+          privateKey,
         });
 
-        if (!publicKeyString) {
+        if (!keyStore) {
           return {
             code: 'xxxx',
-            message: 'publicKeyString error',
+            message: 'keyStore error',
           };
         }
-
-        console.log(`publicKeyString::`, publicKeyString);
-        const publicKeyObject = crypto.createPublicKey(publicKeyString);
-
-        console.log(`publicKeyObject::`, publicKeyObject);
 
         // created token pair
         const tokens = await createTokenPair(
           { userId: newShop._id, email },
-          publicKeyString,
+          publicKey,
           privateKey,
         );
 

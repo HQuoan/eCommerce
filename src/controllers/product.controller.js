fbf06e08d09cdda58a8364315b2b13fd0e4f056c
Vo Minh Huy <huyvodtan@gmail.com>
2024-04-01 21:27:13 +0700

    ss15 service product with factory and strategy

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,12 @@
-const ProductService = require('../services/product.service');
 const { SuccessResponse } = require('../core/success.response');
+// const ProductService = require('../services/product.service');
+const ProductServiceV2 = require('../services/product.service.xxx');
 
 class ProductController {
   async createProduct(req, res, next) {
     return new SuccessResponse({
       message: 'Create new Product success!',
-      metadata: await ProductService.createProduct(req.body.product_type, {
+      metadata: await ProductServiceV2.createProduct(req.body.product_type, {
         ...req.body,
         product_shop: req.user.userId,
       }),

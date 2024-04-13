68a16c0f6d36946eca81be311a13cc17c187dc6c
Vo Minh Huy <huyvodtan@gmail.com>
2024-04-14 00:29:55 +0700

    ss22 discount service: amount, delete, cancel

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,98 +1,98 @@
-// /* eslint-disable no-use-before-define */
-// /* eslint-disable camelcase */
-// /* eslint-disable max-classes-per-file */
+/* eslint-disable no-use-before-define */
+/* eslint-disable camelcase */
+/* eslint-disable max-classes-per-file */
 
-// const { BadRequestError } = require('../core/error.response');
-// const { product, clothing, electronic } = require('../models/product.model');
-// const { insertInventory } = require('../models/repositories/inventory.repo');
+const { BadRequestError } = require('../core/error.response');
+const { product, clothing, electronic } = require('../models/product.model');
+const { insertInventory } = require('../models/repositories/inventory.repo');
 
-// class ProductFactory {
-//   /*
-//     type: 'Clothing'
-//     payload:
-//   */
-//   static async createProduct(type, payload) {
-//     switch (type) {
-//       case 'Clothing':
-//         return new Clothing(payload).createProduct();
-//       case 'Electronic':
-//         return new Electronic(payload).createProduct();
-//       default:
-//         throw new BadRequestError(`Invalid Product Type: ${type}`);
-//     }
-//   }
-// }
+class ProductFactory {
+  /*
+    type: 'Clothing'
+    payload:
+  */
+  static async createProduct(type, payload) {
+    switch (type) {
+      case 'Clothing':
+        return new Clothing(payload).createProduct();
+      case 'Electronic':
+        return new Electronic(payload).createProduct();
+      default:
+        throw new BadRequestError(`Invalid Product Type: ${type}`);
+    }
+  }
+}
 
-// class Product {
-//   constructor({
-//     product_name,
-//     product_thumb,
-//     product_description,
-//     product_quantity,
-//     product_price,
-//     product_type,
-//     product_shop,
-//     product_attributes,
-//   }) {
-//     this.product_name = product_name;
-//     this.product_thumb = product_thumb;
-//     this.product_description = product_description;
-//     this.product_quantity = product_quantity;
-//     this.product_price = product_price;
-//     this.product_type = product_type;
-//     this.product_shop = product_shop;
-//     this.product_attributes = product_attributes;
-//   }
+class Product {
+  constructor({
+    product_name,
+    product_thumb,
+    product_description,
+    product_quantity,
+    product_price,
+    product_type,
+    product_shop,
+    product_attributes,
+  }) {
+    this.product_name = product_name;
+    this.product_thumb = product_thumb;
+    this.product_description = product_description;
+    this.product_quantity = product_quantity;
+    this.product_price = product_price;
+    this.product_type = product_type;
+    this.product_shop = product_shop;
+    this.product_attributes = product_attributes;
+  }
 
-//   // create new product
-//   async createProduct(product_id) {
-//     const newProduct = await product.create({ ...this, _id: product_id });
-//     console.log('newproduct:::', newProduct);
-//     if (newProduct) {
-//       console.log('dfdsfasdf;');
+  // create new product
+  async createProduct(product_id) {
+    const newProduct = await product.create({ ...this, _id: product_id });
+    console.log('newproduct:::', newProduct);
+    if (newProduct) {
+      console.log('dfdsfasdf;');
 
-//       // add product_stock in inventory collection
-//       await insertInventory({
-//         productId: newProduct._id,
-//         shopId: this.product_shop,
-//         stock: this.product_quantity,
-//       });
-//     }
+      // add product_stock in inventory collection
+      await insertInventory({
+        productId: newProduct._id,
+        shopId: this.product_shop,
+        stock: this.product_quantity,
+      });
+    }
 
-//     return newProduct;
-//   }
-// }
+    return newProduct;
+  }
+}
 
-// class Clothing extends Product {
-//   async createProduct() {
-//     const newClothing = await clothing.create({
-//       ...this.product_attributes,
-//       product_shop: this.product_shop,
-//     });
-//     if (!newClothing) throw new BadRequestError('Create new clothing error');
+class Clothing extends Product {
+  async createProduct() {
+    const newClothing = await clothing.create({
+      ...this.product_attributes,
+      product_shop: this.product_shop,
+    });
+    if (!newClothing) throw new BadRequestError('Create new clothing error');
 
-//     const newProduct = await super.createProduct(newClothing._id);
-//     if (!newProduct) throw new BadRequestError('Create new product error');
+    const newProduct = await super.createProduct(newClothing._id);
+    if (!newProduct) throw new BadRequestError('Create new product error');
 
-//     return newProduct;
-//   }
-// }
+    return newProduct;
+  }
+}
 
-// class Electronic extends Product {
-//   async createProduct() {
-//     const newElectronic = await electronic.create({
-//       ...this.product_attributes,
-//       product_shop: this.product_shop,
-//     });
+class Electronic extends Product {
+  async createProduct() {
+    const newElectronic = await electronic.create({
+      ...this.product_attributes,
+      product_shop: this.product_shop,
+    });
 
-//     if (!newElectronic)
-//       throw new BadRequestError('Create new Electronic error');
+    if (!newElectronic)
+      throw new BadRequestError('Create new Electronic error');
 
-//     const newProduct = await super.createProduct(newElectronic._id);
-//     if (!newProduct) throw new BadRequestError('Create new product error');
+    const newProduct = await super.createProduct(newElectronic._id);
+    if (!newProduct) throw new BadRequestError('Create new product error');
 
-//     return newProduct;
-//   }
-// }
+    return newProduct;
+  }
+}
 
-// module.exports = ProductFactory;
+module.exports = ProductFactory;
